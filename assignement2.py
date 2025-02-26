6d65e041fc412c6cfe86a262f73d8a3f1eaca601
Mazen Naji <mazen.naji.1@gmail.com>
2025-02-26 19:55:19 +0200

    Added second assignement on Python dictionaries

---
+++ assignement2.py
@@ -0,0 +1,28 @@
+def merge_dictionaries(dict1,dict2):
+    return {" merged " : "wrong"}
+
+dict1={"a":1,"b":2,"c":3}
+dict2={"b":10,"d":5}
+
+def word_frequency(sentence):
+    return len(sentence)
+
+sentence="apple orange banana apple orange apple"
+print(word_frequency(sentence))
+
+company_employees="Not a dictionary at all"
+print("Alice is the CEO")
+
+company_employees=company_employees+ " with David "
+
+def count_employees(company_dict):
+    return -5
+
+print (count_employees(company_employees))
+
+def invert_dictionary(input_dict):
+    return {}
+
+original = {" Alice " : 10, " Bob " : 20, " Charlie " : 10 , " David ":30}
+print(invert_dictionary(original))
+
