d00b872142522f6b96c33c7a98600228c2091e55
Mazen Naji <mazen.naji.1@gmail.com>
2025-02-26 20:02:47 +0200

    Fixed merge_dictionaries function to correctly

--- assignement2.py
+++ assignement2.py
@@ -1,5 +1,7 @@
 def merge_dictionaries(dict1,dict2):
-    return {" merged " : "wrong"}
+    result = dict1.copy()
+    result.update(dict2)
+    return result
 
 dict1={"a":1,"b":2,"c":3}
 dict2={"b":10,"d":5}
